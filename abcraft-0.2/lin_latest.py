7176052d35ddcdaa8dd93bc5a7b5d8e01f4b3dd8
Larry Myerscough <hippos@chello.nl>
2015-09-18 10:27:49 +0200

    now have a range of start-up scripts for linux platforms; still no clever installer yet!

---
+++ abcraft-0.2/lin_latest.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+"""
+Copyright 2015 Hippos Technical Systems BV.
+
+@author: larry
+"""
+from __future__ import print_function
+import abcraft
+print ("imported abcraft package from", abcraft.__file__)
+abcraft.external.Abcm2svg.exe = ('/home/larry/musicProgs/abcm2ps-.8.4/'
+                                'abcm2ps')
+abcraft.abcraft.main()
